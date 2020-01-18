626580c21af59da9c412a630f9157827a25d13d8
Colin Doncaster <colin@peregrinelabs.com>
2020-01-18 13:17:33 -0500

    - updated prman lib reference on Windows.

--- ecosystem/dev/cmake/FindPrman.cmake
+++ ecosystem/dev/cmake/FindPrman.cmake
@@ -4,14 +4,14 @@
 #
 # The coded instructions, statements, computer programs, and/or related
 # material (collectively the "Data") in these files contain unpublished
-# information proprietary to Peregrine Visual Storytelling Ltd. ("Peregrine") 
-# and/or its licensors, which is protected by U.S. and Canadian federal 
+# information proprietary to Peregrine Visual Storytelling Ltd. ("Peregrine")
+# and/or its licensors, which is protected by U.S. and Canadian federal
 # copyright law and by international treaties.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND. PEREGRINE
 # DOES NOT MAKE AND HEREBY DISCLAIMS ANY EXPRESS OR IMPLIED WARRANTIES
 # INCLUDING, BUT NOT LIMITED TO, THE WARRANTIES OF NON-INFRINGEMENT,
-# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, OR ARISING FROM A COURSE 
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, OR ARISING FROM A COURSE
 # OF DEALING, USAGE, OR TRADE PRACTICE. IN NO EVENT WILL PEREGRINE AND/OR ITS
 # LICENSORS BE LIABLE FOR ANY LOST REVENUES, DATA, OR PROFITS, OR SPECIAL,
 # DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES, EVEN IF PEREGRINE AND/OR ITS
@@ -33,8 +33,8 @@ IF ( PRMAN_Base_Dir )
 
 	MESSAGE(STATUS "Found Prman: ${PRMAN_Base_Dir}")
 
-	IF ( WIN32 )	
-		SET ( PRMAN_LIB libprman )
+	IF ( WIN32 )
+		SET ( PRMAN_LIB "libprman.lib" )
 	ELSE ( WIN32 )
 		SET ( PRMAN_LIB prman )
 	ENDIF ( WIN32 )
@@ -44,7 +44,7 @@ IF ( PRMAN_Base_Dir )
   	SET ( PRMAN_LIBRARY_DIRS ${PRMAN_Base_Dir}/lib
     	CACHE STRING "Prman library directories")
   	SET ( PRMAN_FOUND TRUE )
-	
+
 	LINK_DIRECTORIES( ${PRMAN_LIBRARY_DIRS} )
 ELSE ( PRMAN_Base_Dir )
 	MESSAGE ( WARNING "Prman not found!")
